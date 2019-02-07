6e6564337031e7644522722f158124dfdabbc90c
Emre Edemir <edemiremrewokrs@gmail.com>
2019-02-07 19:09:27 +0300

    Day1.1

---
+++ Othello/Assets/Script/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+
+
+
+
+
+    public void Start()
+    {
+
+        InitiliazeGame();
+    }
+
+
+    public void InitiliazeGame()
+    {
+
+
+
+    }
+    public void StartGame()
+    {
+        
+    }
+
+
+    public void ResetGame()
+    {
+
+
+    }
+
+
+    public void ExitGame()
+    {
+
+
+    }
+
+}
