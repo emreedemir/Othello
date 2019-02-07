f7da0ee5d37fd025ccf3d818706a43c95df8c962
Emre Edemir <edemiremrewokrs@gmail.com>
2019-02-07 18:56:25 +0300

    Day1.1

---
+++ Othello/Assets/Script/Board.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour {
+
+
+
+    public Cell cell;
+
+    List<Cell> cells = new List<Cell>();
+
+    
+    int b_width = 8;
+    int b_height =8;
+
+    Cell[,] allCells;
+     
+
+    public void Start()
+    {
+ 
+        allCells = new Cell[b_width,b_height];
+
+        CreateBoard();
+
+    }
+
+    public void CreateBoard()
+    {
+        int f_x = 0;
+        for (int i = 0; i < b_width; i++)
+        {
+            for (int j = 0; j < b_height; j++)
+            {
+                Cell cellInstantie = Instantiate(cell, Vector3.zero, Quaternion.identity);
+                
+                allCells[i, j] = cellInstantie;
+                cellInstantie.transform.name = "Cell[" + i + "," + j + "]";
+                f_x++;
+                cellInstantie.SetCell(i, j);
+            }
+        }
+
+    }
+
+
+
+
+    
+ 
+}
---
+++ Othello/Assets/Script/Cell.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour {
+
+
+
+    int x_index;
+    int y_index;
+
+    float bias = .055f;
+
+    public int c_value;
+   
+    Color c_green = Color.green;
+    Color c_white = Color.white;
+
+    Vector3 cellPosition;
+
+    Vector3 scaleCell = new Vector3(5,5,5);
+
+    Renderer renderer;
+
+    public enum CellType
+    {
+        Green =0,
+        White=1
+    }
+
+    public void SetCell(int x,int y)
+    {
+        x_index = x;
+        y_index = y;
+
+        renderer = GetComponent<Renderer>();
+        cellPosition =bias*( new Vector3(x_index,0,y_index));
+        transform.position = cellPosition;
+        transform.localScale = scaleCell;
+
+        renderer.material.color = c_green;
+
+    }
+}
