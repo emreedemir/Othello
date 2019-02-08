fc7560cb3690095122e5bda233b4bdf107c0be2c
Emre Edemir <edemiremrewokrs@gmail.com>
2019-02-08 18:51:16 +0300

    Day2.1

--- Othello/Assets/Script/Piece.cs
+++ Othello/Assets/Script/Piece.cs
@@ -4,13 +4,40 @@ using UnityEngine;
 
 public class Piece : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public enum PieceType
+    {
+        DefaultPiece=0,
+        WhitePiece =1,
+        BlackPiece=2
+    }
+
+    public PieceType pieceType = PieceType.DefaultPiece;
+    Color white = Color.white;
+    Color black = Color.black;
+    Color red = Color.red;//Default color
+
+    Renderer renderer;
+
+    public void SetPosition(Vector3 position)
+    {
+        renderer = GetComponent<Renderer>();
+        transform.position = position;
+        SetColor();
+    }
+
+    public void SetColor()
+    {
+        if (pieceType == PieceType.BlackPiece)
+        {
+            renderer.material.color = black;
+        }
+        else if (pieceType == PieceType.WhitePiece)
+        {
+            renderer.material.color = white;
+        }
+        else
+        {
+            renderer.material.color = red;
+        }
+    }
 }
---
+++ Othello/Assets/Script/PiecePool.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PiecePool : MonoBehaviour {
+
+    public Piece m_piece;
+    List<Piece> m_whitePieces = new List<Piece>();
+    List<Piece> m_blackPieces = new List<Piece>();
+    int m_countPiece = 64;
+
+    Piece.PieceType blackType = Piece.PieceType.BlackPiece;
+    Piece.PieceType whiteType = Piece.PieceType.WhitePiece;
+
+
+    Vector3 poolPosition = new Vector3(100,100,100);
+
+    
+    public void Start()
+    {
+        InitiliazePool(m_whitePieces,whiteType);
+        InitiliazePool(m_blackPieces,blackType);
+    }
+
+    public void InitiliazePool(List<Piece> pieceList,Piece.PieceType pieceType)
+    {
+        for (int i = 0; i < m_countPiece; i++)
+        {
+            Piece instantiePiece = Instantiate(m_piece,poolPosition,Quaternion.identity);
+            instantiePiece.pieceType =pieceType;
+            instantiePiece.transform.name = "Piece[" + i + "]"+pieceType.ToString();
+            pieceList.Add(instantiePiece);
+        }
+    }
+}
+
+
+
+
+
+
+
