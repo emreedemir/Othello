f80d8cec3d410b9951124ed99715f0b6a2ea1dc3
Onur Tanrıkulu <onurtnrkl@gmail.com>
2019-02-10 18:05:42 +0100

    Player classes refactored.
    * AIPlayer and Player now inherit from BasePlayer.

---
+++ Assets/Script/BasePlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class BasePlayer : MonoBehaviour
+{
+    private Piece[,] pieces;
+    private List<Piece> m_listOfPieceS = new List<Piece>();
+
+    virtual protected void Start()
+    {
+        pieces = new Piece[8, 8];
+    }
+
+    public void AddPiece(Piece piece, int xIndex, int yIndex)
+    {
+        pieces[xIndex, yIndex] = piece;
+        m_listOfPieceS.Add(piece);
+    }
+
+    public void RemovePiece(Piece piece, int xIndex, int yIndex)
+    {
+        pieces[xIndex, yIndex] = null;
+        m_listOfPieceS.Remove(piece);
+    }
+}
